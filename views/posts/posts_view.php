942628eaeca853e85446e2379710fe7d49c4e4df
jyrgenhallast <jurgen.hallast@khk.ee>
2018-06-08 16:48:41 +0300

    Kommentaaride lisamine posts_view

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,23 +1,36 @@
 <div class="container">
-	<div class="well">
-		<div class="media">
-			<div class="media-body">
-				<h4 class="media-heading"><?php echo $post['post_subject'];?></h4>
-				<p class="text-right"><?php echo $post['name'];?></p>
-				<p><?php echo $post['post_text'];?></p>
-				<ul class="list-inline list-unstyled">
-					<li><span><i class="glyphicon glyphicon-calender"></i><?php echo $post['post_created'];?></span></li>
-					<li></li>
-					<?php foreach($tags as $tag):?>
-						<a href="#">
-                        <span class="label label-info">
-                           <?php echo $tag['tag_name'];?>
-                        </span>
-						</a>&nbsp;
-					<?php endforeach; ?>
-					<li></li>
-					<span><i class="glyphicon glyphicon-comment"></i>? comment</span>
-				</ul>
-			</div>
-		</div>
-	</div>
+
+    <div class="row">
+        <div class="col-lg-8">
+
+            <h1><?=$post['post_subject'];?></h1>
+            <p class="lead"><i class="fa fa-user"></i> by <?=$post['name'];?>
+            </p>
+            <hr>
+            <p><i class="fa fa-calendar"></i> Posted on <?=$post['post_created'];?></p>
+            <p><i class="fa fa-tags"></i> Tags: <?foreach ($tags as $tag):?><a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><span class="badge badge-info"><?=$tag['tag_name']?></span></a> <?endforeach?></p>
+
+            <hr>
+            <p><?=$post['post_text'];?></p>
+        </div>
+    </div>
+
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="media comment-box">
+            <div class="media-body">
+
+				<? foreach($comments as $comment): ?>
+                    <h4 class="media-heading"><?=$comment['comment_subject']?></h4>
+                    <p><?=$comment['comment_text']?></p>
+                    <span class="badge badge-success">Commented on <?=$comment['comment_created']?></span> <span class="badge badge-success">Author: <?=$comment['comment_author']?></span>
+
+                    <hr>
+				<? endforeach ?>
+
+            </div>
+        </div>
+    </div>
+</div>
