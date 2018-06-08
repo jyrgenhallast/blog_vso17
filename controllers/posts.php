81032ceec15c966efd5cc98dd388770d9eabc9f9
jyrgenhallast <jurgen.hallast@khk.ee>
2018-06-08 16:54:25 +0300

    Kommentaaride ja tag'de funkstsioonid

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,60 +1,55 @@
 <?php namespace App;
-
 class posts extends Controller
 {
-
-    /**
-     * This is a normal action which will be called when user visits /posts/index URL. Since index is the default
-     * action name, it may be omitted (URL can be /posts). Since posts is by default the default controller, it may
-     * be omitted (URL can be /). After calling the action, a view from views/controller-name/controller-name_action-name.php
-     * is loaded (it must exist, unless the function ends with stop() call.
-     */
-    function index()
-    {
-
-        $this->users = get_all("SELECT * FROM users");
-    }
-
-	function view(){
-		$post_id = $this->params[0];
-		$this->post = get_one("SELECT * FROM posts NATURAL JOIN user WHERE post_id='$post_id'");
-	}
-
-
 	/**
-     * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
-     */
-    function AJAX_success()
-    {
-
-
-        stop(201,'Everything is awesome');
-    }
-
-    /**
-     * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
-     */
-    function AJAX_error()
-    {
-
-        // Test sending emails
-        Mail::send(DEVELOPER_EMAIL, 'test', 'test');
-
-        echo "This text comes from the server and will be shown only in development environment for debugging purposes. ";
-        echo "Here is a nice exception for you to debug:";
-
-        // Generate error for testing
-        throw new \Exception('This is a test');
-
-
-    }
-
-    /**
-     * This function will only be ran in case of POST request
-     */
-    function POST_index()
-    {
-        echo "\$_POST:<br>";
-        var_dump($_POST);
-    }
+	 * This is a normal action which will be called when user visits /posts/index URL. Since index is the default
+	 * action name, it may be omitted (URL can be /posts). Since posts is by default the default controller, it may
+	 * be omitted (URL can be /). After calling the action, a view from views/controller-name/controller-name_action-name.php
+	 * is loaded (it must exist, unless the function ends with stop() call.
+	 */
+	function index()
+	{
+		$this->posts = get_all("SELECT * FROM post");
+		$_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
+		foreach($_tags as $tag){
+			$this->tags[$tag['post_id']][] = $tag;
+		}
+	}
+	/**
+	 * Single-view for the blog posts
+	 */
+	function view()
+	{
+		$post_id = $this->params[0];
+		$this->post = get_first("SELECT * FROM post NATURAL JOIN users WHERE post_id='$post_id'");
+		$this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+		$this->comments = get_all("SELECT * FROM post_comments NATURAL JOIN comment WHERE post_id='$post_id'");
+	}
+	/**
+	 * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
+	 */
+	function AJAX_success()
+	{
+		stop(201,'Everything is awesome');
+	}
+	/**
+	 * This function will only be ran in case of an AJAX request. No view will be attempted to load after this function.
+	 */
+	function AJAX_error()
+	{
+		// Test sending emails
+		Mail::send(DEVELOPER_EMAIL, 'test', 'test');
+		echo "This text comes from the server and will be shown only in development environment for debugging purposes. ";
+		echo "Here is a nice exception for you to debug:";
+		// Generate error for testing
+		throw new \Exception('This is a test');
+	}
+	/**
+	 * This function will only be ran in case of POST request
+	 */
+	function POST_index()
+	{
+		echo "\$_POST:<br>";
+		var_dump($_POST);
+	}
 }
